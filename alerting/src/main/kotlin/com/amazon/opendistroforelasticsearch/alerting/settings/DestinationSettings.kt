c046b3814c531be3b35baa5740ce8c6e8ec5de3a
David Chauvière <david.chauviere@orange.com>
2020-03-27 00:20:34 +0100

    Add loading of mail settings

---
+++ alerting/src/main/kotlin/com/amazon/opendistroforelasticsearch/alerting/settings/DestinationSettings.kt
@@ -0,0 +1,24 @@
+package com.amazon.opendistroforelasticsearch.alerting.settings
+
+import com.amazon.opendistroforelasticsearch.alerting.AlertingPlugin
+import com.amazon.opendistroforelasticsearch.alerting.settings.DestinationMailSettings
+import org.elasticsearch.common.settings.Settings
+import java.io.IOException
+
+/**
+ * settings specific to mail destination.
+ */
+data class DestinationSettings(
+  val mail: DestinationMailSettings
+) {
+    companion object {
+        @JvmStatic
+        @Throws(IOException::class)
+        fun parse(settings : Settings): DestinationSettings {
+            val prefix : (String) -> (Boolean) = {it.startsWith("opendistro.alerting.destination.mail")}
+            return DestinationSettings(
+                DestinationMailSettings.parse(settings.filter(prefix))
+            )
+        }
+    }
+}
